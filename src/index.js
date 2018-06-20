05cd7dd1f28fe85c7faefeb999dde7937fcb82c9
Jose Sanchez <sanchezmartinezjosecarlos@gmail.com>
2018-06-20 16:43:57 +0100

    Header component created

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ import App from "./components/App";
 const config = {
   api: {
     weather: {
-      apiKey: "YOUR_API_KEY",
+      apiKey: "b7aecc81c3f01e1eadcb367a666606c4",
       city: "London",
       url: "https://api.openweathermap.org/data/2.5/weather"
     },
     unsplash: {
-      apiKey: "YOUR_API_KEY",
+      apiKey: "217d39354a152114b5bb4e0f77e4ca3c7ecdbe155bb9d43580e11138def7915a",
       url: "https://api.unsplash.com/search/photos"
     }
   }
